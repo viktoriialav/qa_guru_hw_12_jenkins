e6380eba7ec810d0c262de7898725ab6f4a73426
Viktoriia Lavrova <viktoriia.lav2908@gmail.com>
2024-07-17 22:18:00 +0300

    Change browser settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,29 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function", autouse=True)
-def browser_management():
+def setup_browser(request):
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_height = 1080
     browser.config.window_width = 1920
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
+    options = Options()
+    options.page_load_strategy = 'eager'
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(command_executor=f'https://user1:1234@selenoid.autotests.cloud/wd/hub',
+                              options=options)
+    browser.config.driver = driver
 
-    yield
+    yield browser
 
     browser.quit()
